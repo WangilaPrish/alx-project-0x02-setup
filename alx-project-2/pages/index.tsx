101052206d26f35b642275fc1e0fc6d7842f432e
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-03 16:34:53 +0300

    Refactor index page to remove Header component and simplify structure

--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,13 +1,9 @@
-import Header from '../components/layout/Header'
+import React from 'react'
 
-const HomePage = () => (
-    <>
-        <Header />
-        <main className="p-4">
-            <h1 className="text-3xl font-bold">Welcome to SoloForge!</h1>
-            <p className="mt-2">Explore posts and users fetched from external APIs.</p>
-        </main>
-    </>
-)
+const index = () => {
+    return (
+        <div>index</div>
+    )
+}
 
-export default HomePage
+export default index
